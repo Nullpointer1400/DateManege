07012bc740d517e273931af8b444bcaaee1cb60c
KusogeMuseum <34642401+KusogeMuseum@users.noreply.github.com>
2018-03-14 16:32:32 +0900

    1st
    1st

---
+++ DateTest _test/Assets/DateManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DateManager : MonoBehaviour {
+	private string url = "http://サーバーのURL/Date.php";
+	//json化したデータの読み込み準備
+	public class time
+	{
+		public int Year;
+		public int Month;
+		public int Day;
+		public int Hour;
+		public int Minute;
+		public int Second;
+	}
+	private Text text;
+	//jsonを読み込むコルーチンをスタート
+	void Start () {
+		text = gameObject.GetComponent<Text> ();
+		StartCoroutine (Date ());
+	}
+	//jsonを読み込むコルーチン
+	IEnumerator Date (){
+		//WWWクラスでファイルに接続
+		WWW www = new WWW (url);
+
+			yield return www;
+		//json化したファイルをstring型の変数に直す
+		string json = www.text;
+		//上のstring型の変数をjsonutillityでjsonとして読み込む
+		time ntime = JsonUtility.FromJson<time> (json);
+		//日付として使用できる変数
+		string DateTime =ntime.Year+":"+ntime.Month+":"+ntime.Day+":"+ntime.Hour+":"+ntime.Minute+":"+ntime.Second;
+		text.text = DateTime;
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine (Date ());
+	}
+}
